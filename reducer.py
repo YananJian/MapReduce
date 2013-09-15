af89aa79e249120f1e217f2a52261ac1f2b4c161
Yanan_Jian <jianyanan@gmail.com>
2013-09-15 16:28:04 -0400

    fixed multiple bugs

--- reducer.py
+++ reducer.py
@@ -6,38 +6,33 @@ from operator import itemgetter
 import sys
 import os
 
-def read_mapper_output(file, separator='\t'):
-    for line in file:
-        yield line.rstrip().split(separator, 1)
-
 def format_print(atc_date, atc_total):
     for atc, date_pv in atc_date.iteritems():
         for date, pv in date_pv.iteritems():
-            print atc_total.get(atc), '\t', atc, '\t', date, ':', pv
+            print (atc_total.get(atc)+'\t'+ atc+'\t'+ date+ ':'+ pv)
 
-def main(separator='\t'):
-    data = read_mapper_output(sys.stdin, separator=separator)
+def main():
     atc_date = {}
     atc_total = {}
-    fname = os.environ["map.input.file"]
-    __date = fname.split('-')[1]
-    for atc, group in groupby(data, itemgetter(0)):
-        try:
+    for line in sys.stdin:
+        line = line.strip()
+        tmp = line.split('\t')
+        if len(tmp) > 2:
+            atc = tmp[0]
+            date = tmp[1]
+            pv = int(tmp[2])
             if atc_total.get(atc):
-                atc_total[atc] += int(count)
+                atc_total[atc] += int(pv)
             else:
-                atc_total[atc] = int(count)
+                atc_total[atc] = int(pv)
             
             if not atc_date.get(atc):
-                atc_date[atc] = ""
+                atc_date[atc] = {}
 
-            if atc_date[atc].get(__date):
-                atc_date[atc][__date] += (int(count) for art, count in group)
+            if atc_date[atc].get(date):
+                atc_date[atc][date] += int(pv)
             else:
-                atc_date[atc][__date] = int(count)
-        except ValueError:
-            pass
+                atc_date[atc][date] = int(pv)
     format_print(atc_date, atc_total)
-
 if __name__ == "__main__":
     main()
